448396c5939b1c1214920f1bea7e0775f9854f9d
Timothy Stott <stott.timothy@gmail.com>
2015-06-02 11:18:47 +0100

    Sinatra application primitive logging
    Logging added only for demonstration purposes

--- sinatra/application.rb
+++ sinatra/application.rb
@@ -1,9 +1,20 @@
+require 'rubygems'
+require 'bundler/setup'
 require 'sinatra/base'
+require 'logstash-logger'
+
+
 
 class Application < Sinatra::Base
   set :bind, '0.0.0.0'
 
   get '/' do
-    'Hello World'
+    logger = LogStashLogger.new([
+      { type: :file, path: '/tmp/logs/production.json', sync: true },
+      { type: :stdout }
+    ])
+    message = "Awesome message #{Time.now.to_i}"
+    logger.info(message)
+    message
   end
 end
